06de80c8b6353382398559430c752b5b7f24a089
ArthurLVidal <arthurodri2017@gmail.com>
2024-09-27 10:04:32 -0300

    Criação da tela de login (Sem funcionalidades por enquanto)

--- lib/screens/auth_screen/auth_screen.dart
+++ lib/screens/auth_screen/auth_screen.dart
@@ -7,8 +7,51 @@ class AuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.tertiary,
-      body: const Center(
-        child: CircularProgressIndicator(),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Flavor Fusion',
+              style: TextStyle(
+                color: Theme.of(context).primaryColor,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+            const SizedBox(height: 20),
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Senha',
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    label: const Text('Entrar'),
+                    icon: const Icon(Icons.login),
+                  ),
+                  const SizedBox(height: 20),
+                  TextButton.icon(
+                    onPressed: () {},
+                    label: const Text('Criar conta'),
+                    icon: const Icon(Icons.add),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
